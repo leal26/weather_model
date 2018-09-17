537071ae885f0777baa55f5bd820489080154a4b
jaschrass <jaschrass@gmail.com>
2018-09-17 14:39:34 -0500

    Organizing Library
    Please don't explode :)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='weather_model',
+      version='0.1',
+      description='Makes pictures from weather data',
+      url='https://github.com/leal26/weather_model',
+      author='Jake Schrass',
+      author_email='jlostinco@tamu.edu',
+      license='MIT',
+      packages=['weather_model'],
+      zip_safe=False)
